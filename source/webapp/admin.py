861820ab55e3a3a3b4338d9070c2ba38ff680d48
gomzan <92199630+gomzan@users.noreply.github.com>
2022-01-13 12:29:13 +0600

    заполнено admin.py

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from models import Todolist
+
+
+class TodolistAdmin(admin.ModelAdmin):
+    list_display = ['id', 'task', 'status', 'deadline']
+    list_filter = ['status']
+    search_fields = ['task', 'deadline']
+    fields = ['task', 'status', 'deadline', 'description']
+
+
+admin.site.register(Todolist, TodolistAdmin)
