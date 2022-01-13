2e9d8de0b594b62aa6546c60e3e67c57f7c48102
gomzan <92199630+gomzan@users.noreply.github.com>
2022-01-13 12:38:37 +0600

    добавлена Миграция

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -7,8 +7,8 @@ status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),
 
 class Todolist(models.Model):
     todolist = models.CharField(max_length=400, null=False, blank=False, verbose_name='Цель')
-    status = models.CharField(max_length=30, null=False, blank=False, default="new", choices=status_choices,
-                              verbose_name="Статус")
+    status = models.CharField(max_length=30, null=False, blank=False, default='active', choices=status_choices,
+                              verbose_name='Статус')
     description = models.TextField(max_length=2000, null=True, blank=False, default="Подробное описание отсутствует",
                                    verbose_name="Описание")
     deadline = models.DateField(null=True, blank=True, default=None, verbose_name="Дата когда задача выполнена")
