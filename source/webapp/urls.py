7b944063c6348d4e9318ced7e6acc513ca586735
gomzan <92199630+gomzan@users.noreply.github.com>
2022-01-13 23:16:27 +0600

    создание urls

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.views.generic import RedirectView
 
 from webapp.models import Todolist
+
 from webapp.views import (
     create_todolist_view,
     TodolistView,
@@ -12,7 +13,7 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('todolist/', RedirectView.as_view(pattern_name='index')),
     path('todolist/add', create_todolist_view, name='todolist_add'),
-    path('todolist/<int:pk>/', RedirectView.as_view, name='todolist_view'),
+    path('todolist/<int:pk>/', TodolistView.as_view(pattern_name='todolist_view'), name='todolist_view'),
     path('todolist/<int:pk>/update', todolist_update_view, name='todolist_update_view'),
     path('todolist/<int:pk>/delete', todolist_delete_view, name='todolist_delete_view')
 
