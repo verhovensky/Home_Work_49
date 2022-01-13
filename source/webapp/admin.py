3ee8b4b75956d002b1ae95f07e0036758b902f0f
gomzan <92199630+gomzan@users.noreply.github.com>
2022-01-13 19:56:31 +0600

    заполнение admin.py

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -7,10 +7,11 @@ from webapp.models import Todolist, Status, Type
 
 
 class TodolistAdmin(admin.ModelAdmin):
-    list_display = ['id', 'summary', 'status', 'created_at', 'updated_at']
+    list_display = ['id', 'summary', 'status', 'created_at']
     list_filter = ['summary']
     search_fields = ['task', 'deadline']
     fields = ['summary', 'status', 'created_at', 'updated_at', 'description']
+    readonly_fields = ['created_at', 'updated_at']
 
 
 admin.site.register(Todolist, TodolistAdmin)
