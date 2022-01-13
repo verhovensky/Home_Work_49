33a18ec38aac1a6473b0332155204e0cf24d356a
gomzan <92199630+gomzan@users.noreply.github.com>
2022-01-13 23:33:19 +0600

    создание urls

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,20 +1,11 @@
 from django.urls import path
-from django.views.generic import RedirectView
-
-from webapp.models import Todolist
-
-from webapp.views import (
-    create_todolist_view,
-    TodolistView,
-    todolist_update_view,
-    todolist_delete_view, IndexView)
+from webapp.views import IndexView, TodolistView, CreateView, UpdateView, DeleteView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('todolist/', RedirectView.as_view(pattern_name='index')),
-    path('todolist/add', create_todolist_view, name='todolist_add'),
-    path('todolist/<int:pk>/', TodolistView.as_view(pattern_name='todolist_view'), name='todolist_view'),
-    path('todolist/<int:pk>/update', todolist_update_view, name='todolist_update_view'),
-    path('todolist/<int:pk>/delete', todolist_delete_view, name='todolist_delete_view')
+    path('todolist/<int:pk>', TodolistView.as_view(), name='todolist_view'),
+    path('add/', CreateView().as_view(), name='todolist_add'),
+    path('delete/<int:pk>', DeleteView().as_view(), name='todolist_delete_view'),
+    path('update/<int:pk>', UpdateView().as_view(), name='todolist_update_view')
 
 ]
