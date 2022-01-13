288aae8f558a29cda97a9383d09d7c5ba24612fd
gomzan <92199630+gomzan@users.noreply.github.com>
2022-01-13 23:32:56 +0600

    добавлины классы

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -14,19 +14,21 @@ class IndexView(View):
         return render(request, 'index.html', {'todolist': todolist})
 
 
-def create_todolist_view(request):
-    if request.method == 'GET':
-        form = TodolistForm()
-        return render(request, 'create_todolist.html', {'form': form})
-    else:
-        form = TodolistForm(data=request.POST)
-        if form.is_valid():
-            summary = form.cleaned_data.get('summary')
-            description = form.cleaned_data.get('description')
-            status = form.cleaned_data.get('status')
-            new_todolist = Todolist.objects.objects.create(summary=summary, description=description, status=status, type=type)
-            return redirect('todolist_view', pk=new_todolist)
-        return render(request, 'create_todolist.html', {'form': form})
+class CreateView(View):
+
+    def create_todolist_view(request):
+        if request.method == 'GET':
+            form = TodolistForm()
+            return render(request, 'create_todolist.html', {'form': form})
+        else:
+            form = TodolistForm(data=request.POST)
+            if form.is_valid():
+                summary = form.cleaned_data.get('summary')
+                description = form.cleaned_data.get('description')
+                status = form.cleaned_data.get('status')
+                new_todolist = Todolist.objects.objects.create(summary=summary, description=description, status=status, type=type)
+                return redirect('todolist_view', pk=new_todolist)
+            return render(request, 'create_todolist.html', {'form': form})
 
 
 class TodolistView(TemplateView):
@@ -38,33 +40,36 @@ class TodolistView(TemplateView):
         return description
 
 
-def todolist_update_view(request, pk):
-    todolist = get_object_or_404(Todolist, pl=pk)
-    if request.method == 'GET':
-        form = TodolistForm(initial={
-            'summary': todolist.summary,
-            'todolist': todolist.todolist,
-            'description': todolist.description,
-            'status': todolist.status,
-            'type': todolist.type
-        })
+class UpdateView(View):
+
+    def todolist_update_view(request, pk):
+        todolist = get_object_or_404(Todolist, pl=pk)
+        if request.method == 'GET':
+            form = TodolistForm(initial={
+                'summary': todolist.summary,
+                'description': todolist.description,
+                'status': todolist.status,
+                'type': todolist.type
+            })
+            return render(request, 'todolist_update.html', {'todolist': todolist, 'form': form})
+        else:
+            form = TodolistForm(data=request.POST)
+            if form.is_valid():
+                todolist.summary = request.POST.get('summary')
+                todolist.description = request.POST.get('description')
+                todolist.status = request.POST.get('status')
+                todolist.type = request.POST.get('type')
+                todolist.save()
+                return redirect('todolist_view', pk=todolist.pk)
         return render(request, 'todolist_update.html', {'todolist': todolist, 'form': form})
-    else:
-        form = TodolistForm(data=request.POST)
-        if form.is_valid():
-            todolist.summary = request.POST.get('summary')
-            todolist.description = request.POST.get('description')
-            todolist.status = request.POST.get('status')
-            todolist.type = request.POST.get('type')
-            todolist.save()
-            return redirect('todolist_view', pk=todolist.pk)
-    return render(request, 'todolist_update.html', {'todolist': todolist, 'form': form})
 
 
-def todolist_delete_view(request, pk):
-    todolist = get_object_or_404(Todolist, pk=pk)
-    if request.method == 'GET':
-        return render(request, 'todolist_delete.html', {'todolist': todolist})
-    else:
-        todolist.delete()
-        return redirect('index')
+class DeleteView(View):
+
+    def todolist_delete_view(request, pk):
+        todolist = get_object_or_404(Todolist, pk=pk)
+        if request.method == 'GET':
+            return render(request, 'todolist_delete.html', {'todolist': todolist})
+        else:
+            todolist.delete()
+            return redirect('index')
