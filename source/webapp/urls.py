4d6b21e1f5cd332af1b19e19eb6283528e291209
gomzan <92199630+gomzan@users.noreply.github.com>
2022-01-13 23:38:35 +0600

    создание urls

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -4,7 +4,7 @@ from webapp.views import IndexView, TodolistView, CreateView, UpdateView, Delete
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('todolist/<int:pk>', TodolistView.as_view(), name='todolist_view'),
-    path('add/', CreateView().as_view(), name='todolist_add'),
+    path('add/<int:pk>', CreateView().as_view(), name='todolist_add'),
     path('delete/<int:pk>', DeleteView().as_view(), name='todolist_delete_view'),
     path('update/<int:pk>', UpdateView().as_view(), name='todolist_update_view')
 
